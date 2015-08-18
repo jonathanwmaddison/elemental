ee7e1e938a18187df69ab753363e12672a2b6b97
jed.watson <jed.watson@773dd515-ab26-cda1-e266-ce57b7db7bff>
2015-08-18 11:17:41 +0000

    v0.3.1
    git-svn-id: https://github.com/elementalui/elemental.git@863 773dd515-ab26-cda1-e266-ce57b7db7bff

---
+++ tags/v0.3.1/src/components/Modal.js
@@ -0,0 +1,107 @@
+var React = require('react/addons');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+var blacklist = require('blacklist');
+var classNames = require('classnames');
+var ReactInterval = require('react-interval');
+var { shouldComponentUpdate } = require('react/lib/ReactComponentWithPureRenderMixin');
+
+const ESC_KEYCODE = 27;
+
+const safeTop = element => {
+	const { innerHeight, pageYOffset, getComputedStyle } = window;
+	const { clientHeight, offsetTop } = element;
+	const { marginBottom, marginTop } = getComputedStyle(element);
+	const [mTop, mBottom] = [parseInt(marginTop, 10), parseInt(marginBottom, 10)];
+	const height = clientHeight + mTop + mBottom;
+
+	if ((offsetTop - mTop) < pageYOffset && offsetTop - mTop + height > pageYOffset + innerHeight) {
+		// Scrolling within modal content, don't move
+		return false;
+	}
+	if (offsetTop - mTop < pageYOffset && height > innerHeight) {
+		// Stick to the window bottom
+		return pageYOffset + innerHeight - height;
+	} else {
+		// Stick to the window top
+		return pageYOffset;
+	}
+};
+
+module.exports = React.createClass({
+	displayName: 'Modal',
+	propTypes: {
+		backdropClosesModal: React.PropTypes.bool,
+		className: React.PropTypes.string,
+		isOpen: React.PropTypes.bool,
+		onCancel: React.PropTypes.func,
+		top: React.PropTypes.number
+	},
+	getInitialState() {
+		return { top: typeof this.props.top !== 'undefined' ? this.props.top : window.pageYOffset };
+	},
+	
+	componentWillReceiveProps: function(nextProps) {
+		if (nextProps.isOpen) {
+			window.addEventListener('keydown', this.handleKeyDown);
+		} else {
+			window.removeEventListener('keydown', this.handleKeyDown);
+		}
+	},
+	
+	handleKeyDown (e) {
+		if ( e.keyCode == ESC_KEYCODE ) {
+			this.props.onCancel();
+		}
+	},
+	
+	shouldComponentUpdate,
+	updateTop() {
+		const top = safeTop(React.findDOMNode(this.refs.dialog));
+		if (top !== false) {
+			this.setState({ top });
+		}
+	},
+	renderDialog() {
+		if (!this.props.isOpen) return null;
+
+		return (
+			<div ref="dialog" className="Modal-dialog" style={{ top: this.state.top }}>
+				<ReactInterval timeout={200} enabled={typeof this.props.top === 'undefined'}
+					callback={this.updateTop} />
+				<div className="Modal-content">
+					{this.props.children}
+				</div>
+			</div>
+		);
+	},
+	renderBackdrop() {
+		if (!this.props.isOpen) return null;
+
+		return <div className="Modal-backdrop" onClick={this.props.backdropClosesModal ? this.props.onCancel : null} />;
+	},
+	render() {
+		// classes
+		var className = classNames('Modal', this.props.className);
+
+		// props
+		var props = blacklist(this.props, 'backdropClosesModal', 'className', 'headerHasCloseButton', 'headerTitle', 'isOpen');
+		props.className = className;
+
+		return (
+			<div {...props}>
+				<ReactCSSTransitionGroup transitionName="Modal-dialog" component="div">
+					{this.renderDialog()}
+				</ReactCSSTransitionGroup>
+				<ReactCSSTransitionGroup transitionName="Modal-background" component="div">
+					{this.renderBackdrop()}
+				</ReactCSSTransitionGroup>
+			</div>
+		);
+	}
+});
+
+
+// expose the children to the top level export
+module.exports.Body = require('./ModalBody');
+module.exports.Footer = require('./ModalFooter');
+module.exports.Header = require('./ModalHeader');
---
+++ tags/v0.3.1/src/components/Pagination.js
@@ -0,0 +1,61 @@
+var React = require('react/addons');
+var blacklist = require('blacklist');
+var classNames = require('classnames');
+
+module.exports = React.createClass({
+	displayName: 'Pagination',
+	propTypes: {
+		className: React.PropTypes.string,
+		onClick: React.PropTypes.func,
+		pagination: React.PropTypes.shape({
+			currentPage: React.PropTypes.number,
+			first:       React.PropTypes.number,
+			last:        React.PropTypes.number,
+			next:        React.PropTypes.bool,
+			pages:       React.PropTypes.array,
+			previous:    React.PropTypes.bool,
+			total:       React.PropTypes.number,
+			totalPages:  React.PropTypes.number
+		})
+	},
+	renderCount() {
+		var count = this.props.pagination;
+		return (
+			<div className="Pagination__count">Showing {count.first} to {count.last} of {count.total}</div>
+		);
+	},
+	renderList() {
+		if (!this.props.pagination.pages) return 'No pages...';
+
+		var self = this;
+
+		var pages = this.props.pagination.pages.map(function(page) {
+			var className = classNames('Pagination__list__item', {
+				'is-selected': self.props.pagination.currentPage === page
+			});
+			return <button key={'page_' + page} className={className} onClick={self.props.onClick}>{page}</button>;
+		});
+
+		return (
+			<div className="Pagination__list">
+				{pages}
+			</div>
+		);
+	},
+	render() {
+		// props
+		var props = blacklist(this.props, 'className');
+
+
+		// classes
+		var componentClass = classNames('Pagination', this.props.className);
+		props.className = componentClass;
+
+		return (
+			<div {...props}>
+				{this.renderCount()}
+				{this.renderList()}
+			</div>
+		);
+	}
+});
