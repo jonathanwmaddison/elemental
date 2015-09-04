39d4df5dbdec0bed8aceb0be5563e24b15e7de7e
jed.watson <jed.watson@773dd515-ab26-cda1-e266-ce57b7db7bff>
2015-09-04 12:44:14 +0000

    v0.4.2
    git-svn-id: https://github.com/elementalui/elemental.git@869 773dd515-ab26-cda1-e266-ce57b7db7bff

---
+++ tags/v0.4.2/src/components/Pagination.js
@@ -0,0 +1,74 @@
+var React = require('react/addons');
+var classNames = require('classnames');
+
+module.exports = React.createClass({
+	displayName: 'Pagination',
+	propTypes: {
+		className: React.PropTypes.string,
+		currentPage: React.PropTypes.number.isRequired,
+		onPageSelect: React.PropTypes.func,
+		pageSize: React.PropTypes.number.isRequired,
+		plural: React.PropTypes.string,
+		singular: React.PropTypes.string,
+		style: React.PropTypes.object,
+		total: React.PropTypes.number.isRequired
+	},
+	renderCount () {
+		let count = '';
+		let { currentPage, pageSize, plural, singular, total } = this.props;
+		if (!total) {
+			count = 'No ' + (plural || 'records');
+		} else if (total > pageSize) {
+			let start = (pageSize * (currentPage - 1)) + 1;
+			let end = Math.min(start + pageSize - 1, total);
+			count = `Showing ${start} to ${end} of ${total}`;
+		} else {
+			count = 'Showing ' + total;
+			if (total > 1 && plural) {
+				count += ' ' + plural;
+			} else if (total === 1 && singular) {
+				count += ' ' + singular;
+			}
+		}
+		return (
+			<div className="Pagination__count">{count}</div>
+		);
+	},
+	onPageSelect (i) {
+		if (!this.props.onPageSelect) return;
+		this.props.onPageSelect(i);
+	},
+	renderPages () {
+		if (this.props.total <= this.props.pageSize) return null;
+
+		let pages = [];
+		let { currentPage, pageSize, total } = this.props;
+
+		for (let i = 0; i < Math.ceil(total / pageSize); i++) {
+			let page = i + 1;
+			let current = (page === currentPage);
+			let className = classNames('Pagination__list__item', {
+				'is-selected': current
+			});
+			/* eslint-disable no-loop-func */
+			pages.push(<button key={'page_' + page} className={className} onClick={() => this.onPageSelect(page)}>{page}</button>);
+			/* eslint-enable */
+		}
+
+		return (
+			<div className="Pagination__list">
+				{pages}
+			</div>
+		);
+
+	},
+	render () {
+		var className = classNames('Pagination', this.props.className);
+		return (
+			<div className={className} style={this.props.style}>
+				{this.renderCount()}
+				{this.renderPages()}
+			</div>
+		);
+	}
+});
