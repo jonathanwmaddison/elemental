5b463162e078469334241e74bee1f981cc90086b
jed.watson <jed.watson@773dd515-ab26-cda1-e266-ce57b7db7bff>
2015-10-12 06:25:50 +0000

    Using a top-level #app div, resolves React 0.14 warning
    git-svn-id: https://github.com/elementalui/elemental.git@765 773dd515-ab26-cda1-e266-ce57b7db7bff

---
+++ branches/feature/react-14/gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var initGulpTasks = require('react-component-gulp-tasks');
+
+var taskConfig = {
+
+	component: {
+		name: 'Elemental',
+		lib: 'lib',
+		dependencies: [
+			'blacklist',
+			'classnames',
+			'react',
+			'react-dom',
+			'react-addons-css-transition-group'
+		],
+		less: {
+			path: 'less',
+			entry: 'elemental.less'
+		}
+	},
+
+	example: {
+		src: 'site/src',
+		dist: 'site/dist',
+		files: [
+			'.gitignore',
+			'404.html',
+			'404.html',
+			'CNAME',
+			'images/*',
+			'index.html',
+			'prism.css',
+			'prism.js'
+		],
+		scripts: [
+			'site.js'
+		],
+		less: [
+			'site.less'
+		]
+	}
+
+};
+
+initGulpTasks(gulp, taskConfig);
