4cd7b4f3ccad3f32ffa0c55b5099ccb4e0d6ab97
jed.watson <jed.watson@773dd515-ab26-cda1-e266-ce57b7db7bff>
2015-12-07 12:23:46 +0000

    v0.5.7
    git-svn-id: https://github.com/elementalui/elemental.git@887 773dd515-ab26-cda1-e266-ce57b7db7bff

---
+++ tags/v0.5.7/src/components/FormInput.js
@@ -0,0 +1,68 @@
+var React = require('react');
+var blacklist = require('blacklist');
+var classNames = require('classnames');
+
+module.exports = React.createClass({
+	displayName: 'FormInput',
+	propTypes: {
+		autofocus: React.PropTypes.bool,
+		className: React.PropTypes.string,
+		disabled: React.PropTypes.bool,
+		href: React.PropTypes.string,
+		id: React.PropTypes.string,
+		multiline: React.PropTypes.bool,
+		name: React.PropTypes.string,
+		noedit: React.PropTypes.bool,
+		onChange: React.PropTypes.func,
+		size: React.PropTypes.oneOf(['lg', 'sm', 'xs']),
+		type: React.PropTypes.string,
+		value: React.PropTypes.oneOfType([
+			React.PropTypes.number,
+			React.PropTypes.string
+		]),
+	},
+
+	getDefaultProps() {
+		return {
+			type: 'text',
+		};
+	},
+
+	componentDidMount () {
+		if (this.props.autofocus) {
+			this.focus();
+		}
+	},
+
+	focus() {
+		this.refs.input.focus();
+	},
+
+	render() {
+		// classes
+		let className = classNames(
+			{
+				'FormInput-noedit': this.props.noedit,
+				'FormInput-noedit--multiline': this.props.noedit && this.props.multiline,
+				'FormInput': !this.props.noedit
+			},
+			(this.props.size ? ('FormInput--' + this.props.size) : null),
+			this.props.className
+		);
+		let props = { ...this.props, className, ref: 'input' };
+		let Element = 'input';
+		if (this.props.noedit && this.props.href) {
+			Element = 'a';
+			props.type = null;
+			props.children = props.children || props.value;
+		} else if (this.props.noedit) {
+			Element = 'div';
+			props.type = null;
+			props.children = props.children || props.value;
+		} else if (this.props.multiline) {
+			Element = 'textarea';
+		}
+
+		return <Element {...props} />;
+	}
+});
