6269c353d447e9e27536cc479981ad21a8b87798
jed.watson <jed.watson@773dd515-ab26-cda1-e266-ce57b7db7bff>
2016-02-16 07:41:55 +0000

    v0.5.12
    git-svn-id: https://github.com/elementalui/elemental.git@880 773dd515-ab26-cda1-e266-ce57b7db7bff

---
+++ tags/v0.5.12/gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var initGulpTasks = require('react-component-gulp-tasks');
+
+var taskConfig = {
+
+	component: {
+		name: 'Elemental',
+		lib: 'lib',
+		dependencies: [
+			'blacklist',
+			'classNames',
+			'react',
+			'react-addons-css-transition-group',
+			'react-dom',
+			'react-router'
+		],
+		less: {
+			path: 'less',
+			entry: 'elemental.less'
+		}
+	},
+
+	example: {
+		src: 'site/src',
+		dist: 'site/dist',
+		files: [
+			'.gitignore',
+			'404.html',
+			'404.html',
+			'CNAME',
+			'images/*',
+			'index.html',
+			'prism.css',
+			'prism.js'
+		],
+		scripts: [
+			'site.js'
+		],
+		less: [
+			'site.less'
+		]
+	}
+
+};
+
+initGulpTasks(gulp, taskConfig);
---
+++ tags/v0.5.12/src/components/Col.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import blacklist from 'blacklist';
+import E from '../constants';
+
+module.exports = React.createClass({
+	displayName: 'Col',
+	propTypes: {
+		/* eslint-disable react/jsx-sort-prop-types */
+		basis: React.PropTypes.oneOfType([
+			React.PropTypes.number, // allow pixels
+			React.PropTypes.string, // allow percentage
+		]),
+		children: React.PropTypes.node,
+		gutter: React.PropTypes.number,
+		style: React.PropTypes.object,
+		lg: React.PropTypes.string, // width as a percentage or fraction
+		md: React.PropTypes.string, // width as a percentage or fraction
+		sm: React.PropTypes.string, // width as a percentage or fraction
+		xs: React.PropTypes.string, // width as a percentage or fraction
+		/* eslint-enable */
+	},
+	getDefaultProps () {
+		return {
+			gutter: E.width.gutter,
+		};
+	},
+	getInitialState: function() {
+		return {
+			windowWidth: (typeof window !== 'undefined') ? window.innerWidth : 0
+		};
+	},
+	componentDidMount: function() {
+		if (typeof window !== 'undefined') window.addEventListener('resize', this.handleResize);
+	},
+	componentWillUnmount: function() {
+		if (typeof window !== 'undefined') window.removeEventListener('resize', this.handleResize);
+	},
+	handleResize: function() {
+		this.setState({
+			windowWidth: (typeof window !== 'undefined') ? window.innerWidth : 0
+		});
+	},
+	render() {
+		let { basis, gutter, xs, sm, md, lg } = this.props;
+		let { windowWidth } = this.state;
+
+		let columnStyle = {
+			minHeight: 1,
+			paddingLeft: (gutter / 2),
+			paddingRight: (gutter / 2),
+		};
+
+		// if no width control is provided fill available space
+		if (!basis && !xs && !sm && !md && !lg) {
+			columnStyle.flex = '1 1 auto';
+			columnStyle.msFlex = '1 1 auto';
+			columnStyle.WebkitFlex = '1 1 auto';
+		}
+
+		// set widths / flex-basis
+		if (basis) {
+			columnStyle.flex = '1 0 ' + basis;
+			columnStyle.msFlex = '1 0 ' + basis;
+			columnStyle.WebkitFlex = '1 0 ' + basis;
+		} else if (windowWidth < E.breakpoint.xs) {
+			columnStyle.width = xs;
+		} else if (windowWidth < E.breakpoint.sm) {
+			columnStyle.width = sm || xs;
+		} else if (windowWidth < E.breakpoint.md) {
+			columnStyle.width = md || sm || xs;
+		} else {
+			columnStyle.width = lg || md || sm || xs;
+		}
+
+		if (!columnStyle.width) {
+			columnStyle.width = '100%';
+		}
+
+		if (columnStyle.width in E.fractions) {
+			columnStyle.width = E.fractions[columnStyle.width];
+		}
+
+		let props = blacklist(this.props, 'basis', 'gutter', 'style', 'xs', 'sm', 'md', 'lg');
+
+		return <div style={Object.assign(columnStyle, this.props.style)} {...props} />;
+	},
+});
